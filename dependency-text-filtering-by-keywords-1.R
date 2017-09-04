d66ce017ce97227de8b8501340603e2cf398b156
Sudhir Voleti <sudhir_voleti@isb.edu>
2017-09-04 16:35:08 +0530

    Update dependency-text-filtering-by-keywords-1.R

--- dependency-text-filtering-by-keywords-1.R
+++ dependency-text-filtering-by-keywords-1.R
@@ -3,10 +3,12 @@ try(require(shiny) || install.packages("shiny"))
 try(require(magrittr) || install.packages("magrittr"))
 try(require(tidytext) || install.packages("tidytext"))
 try(require(dplyr) || install.packages("dplyr"))
+try(require(igraph)|| install.packages("igraph"))
 
 library(NLP)
 library(shiny)
 library(magrittr)
 library(tidytext)
 library(dplyr)
+library(igraph)
 
